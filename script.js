cfd73744b3200b934f6a866828ffa523b5300139
Håkon Bergtun <haakonbergtun@gmail.com>
2025-10-29 15:09:31 +0100

    La til JS-sjekk for bestillingsside og grunnstruktur for variabler

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 if (document.body.contains(document.getElementById("order-form"))) {
   // kun bestilling side js
+  console.log("Jeg lever!");
+
+  const orderForm = document.getElementById("order-form");
+  const plusButtons = document.querySelectorAll(".qty-plus");
+  const minusButtons = document.querySelectorAll(".qty-minus");
+  const totalDisplay = document.getElementById("grand-total");
+  const clearBtn = document.getElementById("clear-order");
+  const tableBtn = document.getElementById("set-table");
+  const tableOutput = document.getElementById("table-output");
 }
