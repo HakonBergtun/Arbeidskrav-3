b23dbbea50eea5a01667edfc7c289f08c792a57a
Mats Sudbo <mats.sudbo@gmail.com>
2025-11-24 12:00:24 +0100

    la til en JS funksjon på index siden. med en Greeting

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ if (document.body.contains(document.getElementById("order-form"))) {
   const tableBtn = document.getElementById("set-table");
   const tableOutput = document.getElementById("table-output");
 }
+// greeting på index
+const greeting = document.getElementById("greeting");
+const hour = new Date().getHours();
+
+if (hour < 12) {
+  greeting.textContent = "God morgen! Klar for dagens første kaffe? ☀️";
+} else if (hour < 18) {
+  greeting.textContent = "God ettermiddag! En kopp til? ☕";
+} else {
+  greeting.textContent = "God kveld! Kaffe før natten? 🌙";
+} 
